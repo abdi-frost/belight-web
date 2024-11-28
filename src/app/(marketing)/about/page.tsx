058b34b93727e7e7d333ff76adc1cf30a33ea49c
abdi-frost <abdimegersa14@gmail.com>
2024-11-28 21:57:38 +0300

    removed container

--- src/app/(marketing)/about/page.tsx
+++ src/app/(marketing)/about/page.tsx
@@ -14,7 +14,7 @@ export default function About() {
 
 const VisionComponent = () => {
   return (
-    <div className="container px-4 md:px-14 lg:px-24 py-10 md:py-18 lg:py-24 md:flex md:items-center md:justify-between">
+    <div className="px-4 md:px-14 lg:px-24 py-10 md:py-18 lg:py-24 md:flex md:items-center md:justify-between">
       <div className="md:w-1/2">
         <h2 className="text-3xl md:text-6xl my-3 md:mb-8 font-semibold mb-4">Vision of Our Company</h2>
         <p className="text-gray-600 mb-6">
@@ -50,7 +50,7 @@ const VisionComponent = () => {
 
 const MissionComponent = () => {
   return (
-    <div className="container px-4 md:px-14 lg:px-24 py-10 md:py-18 lg:py-24 md:flex md:items-center md:justify-between">
+    <div className="px-4 md:px-14 lg:px-24 py-10 md:py-18 lg:py-24 md:flex md:items-center md:justify-between">
       <div className="relative mx-auto max-w-md px-4 sm:max-w-3xl sm:px-6 lg:max-w-none lg:px-0">
         <div className="relative overflow-hidden border">
           <Image src="/vision.png" alt="Image" width={500} height={500} className="w-full object-cover" />
